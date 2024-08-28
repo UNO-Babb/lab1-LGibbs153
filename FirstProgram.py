475effb5daddf41d9923ed414c9310803936b705
LGibbs153 <lgibbs@unomaha.edu>
2024-08-28 16:45:11 +0000

    Update

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,21 +1,23 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Lukas Gibbs
+#Date: 8/28/24
+#Assignment: Lab 1
 
 def main():
-  print("First Program")
+  #print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
-
+  name = input("What is your name? ")
+  print("Good to meet you",name)
   #Use the user's name in the program.
-
+  age = input("How old are you? ")
   #Ask the user for their age.
-
   #Tell the user what year they were born in.
+  age=int(age)
+  birthYear = 2024 - age
   #Assume that they have not had their birthday yet this year.
-
+  print("You were born in" , birthYear)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -6,7 +6,16 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  name1 = input("Please enter a name: ")
+  word1 = input("Please enter a silly word: ")
+  adjective1 = input("Please enter an adjective. An adjective is a word that modifies a noun. : " )
+  noun1 = input("Please enter a noun. A noun is a person, place, or thing. : ")
+  adjective2 = input("Please enter another adjective: ")
+  relative1 = input("Please enter a relative. Please use their relation to you and not their name. For example, grandma and not Suzie. : ")
+  adjective3 = input("Please enter another adjective: ")
+  verb1 = input("Please enter a verb. A verb is an action. : ")
+  adjective4 = input("Please enter another adjective: ")
+  adjective5 = input("Please enter another adjective: ")
   #Print the story with the user supplied words.
 
 
